c0daa09dc52bc95f7f3151691a7275a9e87b3df9
Luiz Otávio <luizomf@gmail.com>
2020-03-28 04:30:50 -0300

    Linear search

--- linear_search/linear_search_O_n.py
+++ linear_search/linear_search_O_n.py
@@ -1,7 +1,18 @@
 """
-Linear Search O(n)
+Linear Search on List[Dict[str, str]] O(n)
+
+Tests:
+>>> person_name_search(people_names, 'Luiz')
+{}
+
+>>> person_name_search(people_names, 'Luiz Miranda')
+{'firstname': 'Luiz', 'lastname': 'Miranda'}
+
+>>> person_name_search(people_names, 'luiz  miranda')
+{'firstname': 'Luiz', 'lastname': 'Miranda'}
 """
 import re
+import doctest
 from typing import List, Dict
 
 people_names: List[Dict[str, str]] = [
@@ -21,8 +32,18 @@ people_names: List[Dict[str, str]] = [
 def person_name_search(
     people_names: List[Dict[str, str]], name_to_find: str
 ) -> Dict[str, str]:
+    """A linear search on List[Dict['firstname', 'lastname']]
+
+    Args:
+        people_names (List[Dict[str, str]]): A list of dicts with names.
+            Dict keys must include first and lastname as first and second keys.
+        name_to_find (str): [description]: A name to find
+
+    Returns:
+        Dict[str, str]: The dict with firstname and lastname of empty dict
+    """
     for person in people_names:
-        firstname, lastname = person.values()
+        firstname, lastname, *_ = person.values()
 
         if name_to_find == f'{firstname} {lastname}':
             return person
@@ -35,8 +56,9 @@ def person_name_search(
 
 
 if __name__ == "__main__":
-    person_1 = person_name_search(people_names, 'helena  Oliveira')
-    person_2 = person_name_search(people_names, 'João barbosa')
+    doctest.testmod(verbose=True)
+    # person_1 = person_name_search(people_names, 'helena  Oliveira')
+    # person_2 = person_name_search(people_names, 'João barbosa')
 
-    print(person_1)
-    print(person_2)
+    # print(person_1)
+    # print(person_2)
