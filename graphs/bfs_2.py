77ae1a7292fdb6b613b5ea10ea4312b66d0fdd98
Luiz Otávio <luizomf@gmail.com>
2020-04-02 01:37:34 -0300

    Breadth-First Search - BFS (with Graphs)

--- graphs/bfs_2.py
+++ graphs/bfs_2.py
@@ -9,24 +9,27 @@ class Vertex:
 
     def __init__(self, name: str) -> None:
         self.name = name
-        self.adjacent_vertices: List[Vertex] = []
+        self.neighbors: List[Vertex] = []
 
-    def add_adjacent(self, *vertex: Vertex) -> None:
+    def add_neighbors(self, *vertex: Vertex) -> None:
         for v in vertex:
-            self.adjacent_vertices.append(v)
+            self.neighbors.append(v)
+
+    def clear(self) -> None:
+        self.neighbors.clear()
 
     def __repr__(self) -> str:
         return f'{self.name} -> ' \
-            f'{", ".join([v.name for v in self.adjacent_vertices])}'
+            f'{", ".join([v.name for v in self.neighbors])}'
 
 
-class DiGraph:
-    """Class Representing a DiGrapth"""
+class Graph:
+    """Class Representing a Grapth"""
 
     def __init__(self) -> None:
         self.graph: Dict[str, Vertex] = {}
 
-    def add_vertex(self, *vertex: Vertex) -> None:
+    def add_vertices(self, *vertex: Vertex) -> None:
         for v in vertex:
             self.graph[v.name] = v
 
@@ -43,70 +46,120 @@ class DiGraph:
 
         while bfs_queue:
             checking_vertex = bfs_queue.popleft()
-            adjacent_vertices = checking_vertex.adjacent_vertices
+            neighbors = checking_vertex.neighbors
 
             bfs_return_value.append(checking_vertex)
 
-            for adjacent_vertex in adjacent_vertices:
-                if adjacent_vertex in visited_vertices:
+            for neighbor in neighbors:
+                if neighbor in visited_vertices:
                     continue
 
-                visited_vertices.append(adjacent_vertex)
-                bfs_queue.append(adjacent_vertex)
+                visited_vertices.append(neighbor)
+                bfs_queue.append(neighbor)
 
         return bfs_return_value
 
 
 def main() -> None:
-    VA = Vertex('A')
-    VB = Vertex('B')
-    VC = Vertex('C')
-    VD = Vertex('D')
-    VE = Vertex('E')
-    VF = Vertex('F')
-    VG = Vertex('G')
-    VH = Vertex('H')
-    VI = Vertex('I')
-    VJ = Vertex('J')
-    VK = Vertex('K')
-    VL = Vertex('L')
-    VM = Vertex('M')
-    VN = Vertex('N')
-    VO = Vertex('O')
-    VP = Vertex('P')
+    list_of_vertices: List[Vertex] = [
+        Vertex('A'), Vertex('B'), Vertex('C'), Vertex('D'),
+        Vertex('E'), Vertex('F'), Vertex('G'), Vertex('H'),
+        Vertex('I'), Vertex('J'), Vertex('K'), Vertex('L'),
+        Vertex('M'), Vertex('N'), Vertex('O'), Vertex('P'),
+        Vertex('Q'), Vertex('R'), Vertex('S'), Vertex('T'),
+        Vertex('U'), Vertex('V'), Vertex('W'), Vertex('X'),
+        Vertex('Y'), Vertex('Z'),
+    ]
 
-    VA.add_adjacent(VF)
-    VB.add_adjacent(VE, VF)
-    VC.add_adjacent(VH)
-    VD.add_adjacent(VG)
-    VE.add_adjacent(VH, VG)
-    VF.add_adjacent(VG, VI, VL)
-    VG.add_adjacent(VJ)
-    VH.add_adjacent(VM)
-    VI.add_adjacent(VK)
-    VJ.add_adjacent(VK)
-    VK.add_adjacent(VN)
-    VL.add_adjacent(VO, VK)
-    VM.add_adjacent(VN)
-    VN.add_adjacent(VP)
-    VO.add_adjacent(VN)
-    VP.add_adjacent(VP)
+    VA = list_of_vertices[0]
+    VB = list_of_vertices[1]
+    VC = list_of_vertices[2]
+    VD = list_of_vertices[3]
+    VE = list_of_vertices[4]
+    VF = list_of_vertices[5]
+    VG = list_of_vertices[6]
+    VH = list_of_vertices[7]
+    VI = list_of_vertices[8]
+    VJ = list_of_vertices[9]
+    VK = list_of_vertices[10]
+    VL = list_of_vertices[11]
+    VM = list_of_vertices[12]
+    VN = list_of_vertices[13]
+    VO = list_of_vertices[14]
+    VP = list_of_vertices[15]
 
-    graph = DiGraph()
-    graph.add_vertex(VA, VB, VC, VD, VE, VF, VG, VH,
-                     VI, VJ, VK, VL, VM, VN, VO, VP)
+    # noqa: F841 - is for in-line ignore the F841 warning from flake8
+    # Because there are a bunch of variables not used bellow.
+    #
+    # Warning F841 is: local variable name is assigned to but never used
+    # http://flake8.pycqa.org/en/2.5.5/warnings.html
 
-    # graph.show()
+    VQ = list_of_vertices[16]  # noqa: F841
+    VR = list_of_vertices[17]  # noqa: F841
+    VS = list_of_vertices[18]
+    VT = list_of_vertices[19]  # noqa: F841
+    VU = list_of_vertices[20]  # noqa: F841
+    VV = list_of_vertices[21]
+    VW = list_of_vertices[22]  # noqa: F841
+    VX = list_of_vertices[23]
+    VY = list_of_vertices[24]  # noqa: F841
+    VZ = list_of_vertices[25]
 
-    bfs_starting_from_a = graph.bfs_algorithm(VA)
+    # For undirected graph
+    VA.add_neighbors(VS, VZ)
+    VZ.add_neighbors(VA)
+    VS.add_neighbors(VA, VX)
+    VX.add_neighbors(VS, VD, VC)
+    VD.add_neighbors(VX, VC, VF)
+    VC.add_neighbors(VX, VD, VF, VV)
+    VF.add_neighbors(VD, VC, VV)
+
+    undirected_graph = Graph()
+    undirected_graph.add_vertices(VA, VZ, VS, VX, VD, VC, VF)
+
+    bfs_undirected_graph_from_s = undirected_graph.bfs_algorithm(VS)
+    print('Undirected Graph')
+    pprint(bfs_undirected_graph_from_s)
+    print()
+
+    # Clear vertices
+    for vertex in list_of_vertices:
+        vertex.clear()
+
+    # For directed graph
+    VA.add_neighbors(VF)
+    VB.add_neighbors(VE, VF)
+    VC.add_neighbors(VH)
+    VD.add_neighbors(VG)
+    VE.add_neighbors(VH, VG)
+    VF.add_neighbors(VG, VI, VL)
+    VG.add_neighbors(VJ)
+    VH.add_neighbors(VM)
+    VI.add_neighbors(VK)
+    VJ.add_neighbors(VK)
+    VK.add_neighbors(VN)
+    VL.add_neighbors(VO, VK)
+    VM.add_neighbors(VN)
+    VN.add_neighbors(VP)
+    VO.add_neighbors(VN)
+    VP.add_neighbors(VP)
+
+    directed_graph = Graph()
+    directed_graph.add_vertices(VA, VB, VC, VD, VE, VF, VG, VH,
+                                VI, VJ, VK, VL, VM, VN, VO, VP)
+
+    print('Directed Graph Starting From A')
+    bfs_starting_from_a = directed_graph.bfs_algorithm(VA)
     pprint(bfs_starting_from_a)
     print()
 
-    bfs_starting_from_b = graph.bfs_algorithm(VB)
+    print('Directed Graph Starting From B')
+    bfs_starting_from_b = directed_graph.bfs_algorithm(VB)
     pprint(bfs_starting_from_b)
     print()
 
-    bfs_starting_from_c = graph.bfs_algorithm(VC)
+    print('Directed Graph Starting From C')
+    bfs_starting_from_c = directed_graph.bfs_algorithm(VC)
     pprint(bfs_starting_from_c)
     print()
 
